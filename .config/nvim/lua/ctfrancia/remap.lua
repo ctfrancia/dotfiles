cc2965356ca3e1a046cb01ba917a9072e1486ee5
Christian Francia <christian.francia@tui.com>
2025-03-07 12:26:40 +0100

    clean up comments in remap.lua

--- .config/nvim/lua/ctfrancia/remap.lua
+++ .config/nvim/lua/ctfrancia/remap.lua
@@ -2,16 +2,8 @@ vim.g.mapleader=" "
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
--- vim.keymap.set("v", "<leader>y", "\"*y")
--- vim.keymap.set("v", "<leader>p", "\"*p")
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
---[[
-vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
-vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
-vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
-vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
-]]
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
