709e62d198111a34daf7f78b3e1aef232f587161
Christian Francia <christian.francia@tui.com>
2025-03-07 08:59:20 +0100

    pre-pull

--- .config/nvim/lua/ctfrancia/lazy/local.lua
+++ .config/nvim/lua/ctfrancia/lazy/local.lua
@@ -1,11 +1,16 @@
 local local_plugin = {
     {
         "mussol",
-        dir = "~/Projects/mussol",
-        disable = true,
-        config = function()
-            -- require("mussol").setup()
-        end,
+        dir = "~/Projects/personal/mussol",
+        opts = {
+            targets = {
+                { name = "TODO",  wt = 2,  fg = "orange", bg = "none" },
+                { name = "FIXME", wt = 10, fg = "yellow", bg = "none" },
+                { name = "BUG",   wt = 8,  fg = "red",    bg = "none" },
+                { name = "NOTE",  wt = 1,  fg = "blue",   bg = "none" },
+            }
+        },
     },
 }
+
 return local_plugin
