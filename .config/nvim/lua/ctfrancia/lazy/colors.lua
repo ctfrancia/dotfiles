2c7a57ddb3dadca9a720b6e90d894ea1ed33611e
Christian Francia <christian.francia@tui.com>
2025-08-11 08:44:10 +0200

    update avante, colors, mason-workaround

--- .config/nvim/lua/ctfrancia/lazy/colors.lua
+++ .config/nvim/lua/ctfrancia/lazy/colors.lua
@@ -1,14 +1,12 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    color = color or "rose-pine-moon"
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
     {
-
         "folke/tokyonight.nvim",
         config = function()
             require("tokyonight").setup({
@@ -27,7 +25,7 @@ return {
                     floats = "dark", -- style for floating windows
                 },
             })
-             ColorMyPencils("tokyonight")
+            ColorMyPencils("tokyonight")
         end
     },
     {
@@ -40,123 +38,7 @@ return {
                     italic = false,
                 },
             })
-
-            -- ColorMyPencils();
+            -- ColorMyPencils("rose-pine-moon")
         end
     },
-    --[[
-    {
-        "erikbackman/brightburn.vim",
-    },
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        opts = {},
-        config = function()
-            ColorMyPencils()
-        end
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
-        config = function()
-            require("gruvbox").setup({
-                terminal_colors = true, -- add neovim terminal colors
-                undercurl = true,
-                underline = false,
-                bold = true,
-                italic = {
-                    strings = false,
-                    emphasis = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true, -- invert background for search, diffs, statuslines and errors
-                contrast = "", -- can be "hard", "soft" or empty string
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = false,
-            })
-        end,
-    },
-    {
-        "folke/tokyonight.nvim",
-        config = function()
-            require("tokyonight").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true, -- Enable this to disable setting the background color
-                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-                styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    -- Background styles. Can be "dark", "transparent" or "normal"
-                    sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
-                },
-            })
-        end
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
-
-            ColorMyPencils();
-        end
-    },
-    {
-        "rebelot/kanagawa.nvim",
-        config = function()
-            require("kanagawa").setup({
-                theme = "lotus",
-                options = {
-                    italic_comments = false,
-                    italic_keywords = false,
-                    italic_functions = false,
-                    italic_variables = false,
-                    contrast = true,
-                    border = true,
-                    popup_border = true,
-                    hide_inactive_statusline = true,
-                },
-            })
-            ColorMyPencils("kanagawa")
-        end
-    }
-    {
-    "savq/melange-nvim",
-    config = function()
-        require("melange").setup({
-            palette = "material",
-            style = "light",
-            contrast = true,
-            border = true,
-            popup_border = true,
-            hide_inactive_statusline = true,
-            italic_comments = false,
-            italic_keywords = false,
-            italic_functions = false,
-            italic_variables = false,
-        })
-        ColorMyPencils("melange")
-    end
-    }
-    ]]
 }
